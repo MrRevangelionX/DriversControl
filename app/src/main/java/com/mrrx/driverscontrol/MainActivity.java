bc591425852ddfd7a65bb35f60bfdccb05d5b62f
MrRX <jpinedatorres@hotmail.com>
2021-12-15 16:56:57 -0600

    Esperando la configuracion del calendario de Android

--- app/src/main/java/com/mrrx/driverscontrol/MainActivity.java
+++ app/src/main/java/com/mrrx/driverscontrol/MainActivity.java
@@ -5,10 +5,12 @@ import static android.widget.Toast.LENGTH_SHORT;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.usage.UsageEvents;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.EventLog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Driver;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     Boolean btnFlag1, btnFlag2, btnFlag3, btnFlag4, btnFlag5, btnFlag6, btnFlag7, btnFlag8, btnFlag9, btnFlag10, btnFlag11, btnFlag12, btnFlag13, btnFlag14
             , btnFlag15, btnFlag16, btnFlag17, btnFlag18, btnFlag19, btnFlag20, btnFlag21, btnFlag22, btnFlag23, btnFlag24, btnFlag25;
     TextView tvRemaining;
+
+    Calendar calendario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +60,9 @@ public class MainActivity extends AppCompatActivity {
         btnFlag24 = false;
         btnFlag25 = false;
 
-        TextView tvRemaining = findViewById(R.id.registro);
-
         setTotalDrivers();
 
-        driversRemain = totalDrivers;
-
+        TextView tvRemaining = findViewById(R.id.registro);
         tvRemaining.setText("Cantidad de Drivers Disponible: " + totalDrivers);
 
         Button btn1 = (Button) findViewById(R.id.btn1);
@@ -88,85 +90,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        Button btn2 = (Button) findViewById(R.id.btn2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(driversRemain > 0 | btnFlag2==true){
-                    if(btnFlag2 == false) {
-                        driversRemain--;
-                        btnFlag2 = true;
-                        btn2.setText("RESERVADO");
-                        btn2.setBackgroundColor(Color.parseColor("#FF0000"));
-                        tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
-                    }else{
-                        if(driversRemain<totalDrivers){
-                            driversRemain++;
-                        }
-                        btnFlag2 = false;
-                        btn2.setText("08:30 AM");
-                        btn2.setBackgroundColor(Color.parseColor("#00FF00"));
-                        tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
-                    }
-                }else{
-                    Toast.makeText(MainActivity.this,"No hay drivers disponibles", LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        Button btn3 = (Button) findViewById(R.id.btn3);
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(driversRemain > 0 | btnFlag3 == true){
-                    if(btnFlag3 == false) {
-                        driversRemain--;
-                        btnFlag3 = true;
-                        btn3.setText("RESERVADO");
-                        btn3.setBackgroundColor(Color.parseColor("#FF0000"));
-                        tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
-                    }else{
-                        if(driversRemain<totalDrivers){
-                            driversRemain++;
-                        }
-                        btnFlag3 = false;
-                        btn3.setText("00:00 AM");
-                        btn3.setBackgroundColor(Color.parseColor("#00FF00"));
-                        tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
-                    }
-                }else{
-                    Toast.makeText(MainActivity.this,"No hay drivers disponibles", LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        Button btn4 = (Button) findViewById(R.id.btn4);
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(driversRemain > 0 | btnFlag4 == true){
-                    if(btnFlag4 == false) {
-                        driversRemain--;
-                        btnFlag4 = true;
-                        btn4.setText("RESERVADO");
-                        btn4.setBackgroundColor(Color.parseColor("#FF0000"));
-                        tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
-                    }else{
-                        if(driversRemain<totalDrivers){
-                            driversRemain++;
-                        }
-                        btnFlag4 = false;
-                        btn4.setText("00:00 AM");
-                        btn4.setBackgroundColor(Color.parseColor("#00FF00"));
-                        tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
-                    }
-                }else{
-                    Toast.makeText(MainActivity.this,"No hay drivers disponibles", LENGTH_SHORT).show();
-                }
-            }
-        });
-
     }
 
     private void setTotalDrivers() {
@@ -179,9 +102,12 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         totalDrivers = Integer.parseInt(inputText.getText().toString());
-                        Toast.makeText(MainActivity.this, totalDrivers, Toast.LENGTH_LONG).show();
+                        driversRemain = totalDrivers;
                     }
                 })
                 .show();
     }
+
+    //CODIGO DE INGRESO DE EVENTOS DEL CALENDARIO DE ANDROID
+
 }
