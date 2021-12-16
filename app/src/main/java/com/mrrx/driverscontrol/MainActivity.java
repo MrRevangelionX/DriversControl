28ea228ce57e456523943006b188791be41f0e88
MrRX <jpinedatorres@hotmail.com>
2021-12-16 11:50:04 -0600

    Agregada la funcion de agendar en calendario, haciendo ultimas pruebas antes de replicar en los botones

--- app/src/main/java/com/mrrx/driverscontrol/MainActivity.java
+++ app/src/main/java/com/mrrx/driverscontrol/MainActivity.java
@@ -13,64 +13,73 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.text.InputType;
+import android.text.format.DateUtils;
 import android.util.EventLog;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.datepicker.DateSelector;
+
 import java.sql.Driver;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     int driversRemain, totalDrivers;
 
-    Boolean btnFlag1, btnFlag2, btnFlag3, btnFlag4, btnFlag5, btnFlag6, btnFlag7, btnFlag8, btnFlag9, btnFlag10, btnFlag11, btnFlag12, btnFlag13, btnFlag14
-            , btnFlag15, btnFlag16, btnFlag17, btnFlag18, btnFlag19, btnFlag20, btnFlag21, btnFlag22, btnFlag23, btnFlag24, btnFlag25;
+    Boolean flagbtn1, flagbtn2, flagbtn3, flagbtn4, flagbtn5, flagbtn6, flagbtn7, flagbtn8, flagbtn9, flagbtn10, flagbtn11, flagbtn12, flagbtn13, flagbtn14
+            , flagbtn15, flagbtn16, flagbtn17, flagbtn18, flagbtn19, flagbtn20, flagbtn21, flagbtn22, flagbtn23, flagbtn24, flagbtn25;
 
     TextView tvRemaining;
 
-    String titulo, ubicacion, descripcion;
+    String titulo, ubicacion, descripcion, date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnFlag1 = false;
-        btnFlag2 = false;
-        btnFlag3 = false;
-        btnFlag4 = false;
-        btnFlag5 = false;
-        btnFlag6 = false;
-        btnFlag7 = false;
-        btnFlag8 = false;
-        btnFlag9 = false;
-        btnFlag10 = false;
-        btnFlag11 = false;
-        btnFlag12 = false;
-        btnFlag13 = false;
-        btnFlag14 = false;
-        btnFlag15 = false;
-        btnFlag16 = false;
-        btnFlag17 = false;
-        btnFlag18 = false;
-        btnFlag19 = false;
-        btnFlag20 = false;
-        btnFlag21 = false;
-        btnFlag22 = false;
-        btnFlag23 = false;
-        btnFlag24 = false;
-        btnFlag25 = false;
+        titulo="Driver Manager Appointment";
+        ubicacion="";
+        descripcion="Se ha solicitado un vehiculo a esta hora";
+
+        flagbtn1 = false;
+        flagbtn2 = false;
+        flagbtn3 = false;
+        flagbtn4 = false;
+        flagbtn5 = false;
+        flagbtn6 = false;
+        flagbtn7 = false;
+        flagbtn8 = false;
+        flagbtn9 = false;
+        flagbtn10 = false;
+        flagbtn11 = false;
+        flagbtn12 = false;
+        flagbtn13 = false;
+        flagbtn14 = false;
+        flagbtn15 = false;
+        flagbtn16 = false;
+        flagbtn17 = false;
+        flagbtn18 = false;
+        flagbtn19 = false;
+        flagbtn20 = false;
+        flagbtn21 = false;
+        flagbtn22 = false;
+        flagbtn23 = false;
+        flagbtn24 = false;
+        flagbtn25 = false;
+
+
 
         setTotalDrivers();
-
-        long epoch = System.currentTimeMillis()/1000;
-        Log.v("Segs",Long.toString(epoch));
 
         TextView tvRemaining = findViewById(R.id.registro);
         tvRemaining.setText("Cantidad de Drivers Disponible: " + totalDrivers);
@@ -79,10 +88,10 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(driversRemain > 0 || btnFlag1 == true){
-                    if(btnFlag1 == false) {
+                if(driversRemain > 0 || flagbtn1 == true){
+                    if(flagbtn1 == false) {
                         driversRemain--;
-                        btnFlag1 = true;
+                        flagbtn1 = true;
                         addEvent();
                         btn1.setText("RESERVADO");
                         btn1.setBackgroundColor(Color.parseColor("#FF0000"));
@@ -91,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                         if(driversRemain<totalDrivers){
                             driversRemain++;
                         }
-                        btnFlag1 = false;
+                        flagbtn1 = false;
                         btn1.setText("08:00 AM");
                         btn1.setBackgroundColor(Color.parseColor("#00FF00"));
                         tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
@@ -101,14 +110,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             private void addEvent() {
+                Calendar calendar;
+                SimpleDateFormat dateFormat;
+
+                calendar = Calendar.getInstance();
+                dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                date = dateFormat.format(calendar.getTime());
+
                 Intent intent = new Intent(Intent.ACTION_INSERT);
+                intent.setType("vnd.android.cursor.item/event");
                 intent.setData(CalendarContract.Events.CONTENT_URI);
-                intent.putExtra(CalendarContract.Events.TITLE,"Driver Manager Appointment");
-                intent.putExtra(CalendarContract.Events.EVENT_LOCATION,"HOME");
-                intent.putExtra(CalendarContract.Events.DESCRIPTION,"A request has been set at this time");
-                intent.putExtra(CalendarContract.Events.ALL_DAY,false);
-                intent.putExtra(CalendarContract.Events.DTSTART, btn1.getText().toString());
-                intent.putExtra(CalendarContract.Events.DURATION, 1800);
+                intent.putExtra(CalendarContract.Events.TITLE,titulo);
+                intent.putExtra(CalendarContract.Events.HAS_ALARM,true);
+                intent.putExtra(CalendarContract.Events.EVENT_LOCATION,ubicacion);
+                intent.putExtra(CalendarContract.Events.DESCRIPTION,descripcion);
+                intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, getMiliseconds(date, btn1.getText().toString()));
+                intent.putExtra(CalendarContract.Events.DURATION, "1800");
                 intent.putExtra(Intent.EXTRA_EMAIL,"revangelionx@gmail.com");
 
                 if(intent.resolveActivity(getPackageManager()) != null){
@@ -121,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setTotalDrivers() {
+    public void setTotalDrivers() {
         final EditText inputText = new EditText(this);
         inputText.setInputType(InputType.TYPE_CLASS_NUMBER);
         new AlertDialog.Builder(MainActivity.this)
@@ -135,5 +152,22 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
                 .show();
+    }
+
+    public long getMiliseconds(String date, String time){
+
+        try{
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm a");
+            Date date1 = sdf.parse(date+" "+time);
+            Calendar cal1 = Calendar.getInstance();
+            cal1.setTime(date1);
+            Calendar beginCal = Calendar.getInstance();
+
+            beginCal.set(cal1.get(Calendar.YEAR),cal1.get(Calendar.MONTH), cal1.get(Calendar.DAY_OF_MONTH), cal1.get(Calendar.HOUR_OF_DAY), cal1.get(Calendar.MINUTE));
+            return beginCal.getTimeInMillis();
+        }
+        catch (Exception e) {
+            return new Date().getTime();
+        }
     }
 }
