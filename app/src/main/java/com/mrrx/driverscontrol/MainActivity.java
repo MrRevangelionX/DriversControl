6e2182b0c27c59cead8466efaa5bfe1e211f5420
MrRX <jpinedatorres@hotmail.com>
2021-12-16 05:15:59 -0600

    Agregada la interaccion con el calendario, solo falta poner la hora de inicio correcta para el evento que se crea y replicar el evento en cada boton

--- app/src/main/java/com/mrrx/driverscontrol/MainActivity.java
+++ app/src/main/java/com/mrrx/driverscontrol/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.usage.UsageEvents;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.text.InputType;
 import android.util.EventLog;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,16 +22,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Driver;
+import java.sql.Time;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     int driversRemain, totalDrivers;
+
     Boolean btnFlag1, btnFlag2, btnFlag3, btnFlag4, btnFlag5, btnFlag6, btnFlag7, btnFlag8, btnFlag9, btnFlag10, btnFlag11, btnFlag12, btnFlag13, btnFlag14
             , btnFlag15, btnFlag16, btnFlag17, btnFlag18, btnFlag19, btnFlag20, btnFlag21, btnFlag22, btnFlag23, btnFlag24, btnFlag25;
+
     TextView tvRemaining;
 
-    Calendar calendario;
+    String titulo, ubicacion, descripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
 
         setTotalDrivers();
 
+        long epoch = System.currentTimeMillis()/1000;
+        Log.v("Segs",Long.toString(epoch));
+
         TextView tvRemaining = findViewById(R.id.registro);
         tvRemaining.setText("Cantidad de Drivers Disponible: " + totalDrivers);
 
@@ -73,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                     if(btnFlag1 == false) {
                         driversRemain--;
                         btnFlag1 = true;
+                        addEvent();
                         btn1.setText("RESERVADO");
                         btn1.setBackgroundColor(Color.parseColor("#FF0000"));
                         tvRemaining.setText("Cantidad de Drivers Disponible: " + driversRemain);
@@ -87,6 +98,24 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }else{
                     Toast.makeText(MainActivity.this,"No hay drivers disponibles", LENGTH_SHORT).show();
+                }
+            }
+            private void addEvent() {
+                Intent intent = new Intent(Intent.ACTION_INSERT);
+                intent.setData(CalendarContract.Events.CONTENT_URI);
+                intent.putExtra(CalendarContract.Events.TITLE,"Driver Manager Appointment");
+                intent.putExtra(CalendarContract.Events.EVENT_LOCATION,"HOME");
+                intent.putExtra(CalendarContract.Events.DESCRIPTION,"A request has been set at this time");
+                intent.putExtra(CalendarContract.Events.ALL_DAY,false);
+                intent.putExtra(CalendarContract.Events.DTSTART, btn1.getText().toString());
+                intent.putExtra(CalendarContract.Events.DURATION, 1800);
+                intent.putExtra(Intent.EXTRA_EMAIL,"revangelionx@gmail.com");
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this,"Evento creado correctamente!", LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"No se pudo crear el evento", LENGTH_SHORT).show();
                 }
             }
         });
@@ -107,7 +136,4 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .show();
     }
-
-    //CODIGO DE INGRESO DE EVENTOS DEL CALENDARIO DE ANDROID
-
 }
